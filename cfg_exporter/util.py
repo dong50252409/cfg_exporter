ca5dd0cf6ccd83288635dfb848265ea5cd3d704b
dong50252409 <304887750@qq.com>
2021-12-16 14:48:01 +0800

    完善trim方法，去除空字符串、回车符、换行符

--- cfg_exporter/util.py
+++ cfg_exporter/util.py
@@ -1,15 +1,12 @@
 import re
 
-re_trim = re.compile(r"\s+")
-re_field_name = re.compile("^[a-zA-Z]([a-zA-Z0-9]|_)*")
+re_trim = re.compile(r"(\s|\n|\r)+")
+re_field_name = re.compile(r"^[a-zA-Z]([a-zA-Z0-9]|_)*")
 
 
 def trim(s):
     """
-    Removes all Spaces contained in the string.
-    If the string is None, an empty string is returned.
-    :param s:
-    :return:
+    去除字符串中所有的空白字符，以及回车符和换行符，如果字段是`None`类型，则返回空字符串.
     """
     if s is None:
         return ""
@@ -18,13 +15,11 @@ def trim(s):
 
 def check_naming(s):
     """
-    Check that the naming conforms to the specification.
-    :param s:str
-    :return:bool
+    检查字符串命名是否符合规范
     """
     if re.search(re_field_name, s):
         return True
     return False
 
 
-__all__ = ('trim', 'check_naming')
+__all__ = 'trim', 'check_naming'
